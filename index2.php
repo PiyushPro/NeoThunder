219de4c1a07cd371796eb7d88a673710c876a9e7
KingMarkA <KingT@armyspy.com>
2015-10-17 00:46:49 +0530

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,34 @@
+<tr><td><img src="http://www.pkmneothunder.com/Carousel.png" style="margin-left:70px;z-index:1;position:absolute;margin-top:30px;opacity:0.5;" height="400px"><div><div id="myCarousel" class="carousel slide" data-ride="carousel"style="margin-left:-50px">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+      <li data-target="#myCarousel" data-slide-to="1"></li>
+      <li data-target="#myCarousel" data-slide-to="2"></li>
+      <li data-target="#myCarousel" data-slide-to="3"></li>
+    </ol>
+
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+      <div class="item active">
+        <img src="http://www.pkmneothunder.com/go_green.png" alt="Chania" width="460" height="345"style="margin-top:104px;border-background: url('light.png')">
+      </div>
+
+      <div class="item"style="width:80%;margin-left:10%">
+        <img src="http://www.pkmneothunder.com/Tom's Pokedex.png" alt="Chania" height="10px" style="margin-top:0px;" >
+      </div>
+    
+      <div class="item">
+        <img src="http://www.pkmneothunder.com/img_flower.jpg" alt="Flower" width="460" height="345">
+      </div>
+
+      <div class="item">
+        <img src="http://www.pkmneothunder.com/img_flower2.jpg" alt="Flower" width="460" height="345">
+      </div>
+    </div></div>
+    <!-- Left and right controls -->
+    
+  </div><br/><center>Pok&eacute;mon Neo Thunder is an Online RPG that welcomes players to develop their intellectual thinking skills in exploring the World of Pok&eacute;mon.<br/>
+  The site is inspired by Pok&eacute;mon Omega and its entire team.<br/>
+  Pok&eacute;mon Neo Thunder bore its seed into the virtual world in June 2014 having an aim to inspire players to think beyond reality.<br/>
+  And guess what?  It's still in development.Hope,you enjoy your time here.<br/>
+  -<u>The PNT Team</u></center></td></tr>
