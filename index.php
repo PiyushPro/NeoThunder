f9e33386f3e7ba2206363c18e7bc8233519eb102
PiyushPro <pshgrwl479@gmail.com>
2015-10-20 23:52:55 -0700

    Update index.php
    Contact me on facebook ^^"
    or Forums. ^^" .You last worked for Destiny ^^" aint it ?

--- index.php
+++ index.php
@@ -32,7 +32,4 @@
 <tr><td><center>Pok&eacute;mon is an idea created and developed by Nintendo.All copyrights and trademarks are reserved with their respective owners.<br/>
 The site is not affliated to GameFreak or Nintendo and its associates.<br/><terms>Terms of use</terms> | Privacy Policy | <id>Credits</id></center></td></tr>
 </table>
-<script src="navigation.js"></script>
-</body><div id="spot-im-root"></div>
-<script type="text/javascript">!function(t,o,p){function e(){var t=o.createElement("script");t.type="text/javascript",t.async=!0,t.src=("https:"==o.location.protocol?"https":"http")+":"+p,o.body.appendChild(t)}t.spotId="1ae8a43e74fef4091707eeb2d5dd964a",t.spotName="",t.allowDesktop=!0,t.allowMobile=!1,t.containerId="spot-im-root",e()}(window.SPOTIM={},document,"//www.spot.im/embed/scripts/launcher.js");</script>
-</html>
+
